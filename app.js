b657812f25fa7d28e131d2163340462d45a445b7
ANTONIO JESUS <antonio.jesus.sanchez.colmenero@everis.com>
2020-07-20 14:46:12 +0200

    Adaptacion heroku

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use(require('./routes/index'));
 
 
-app.listen(3000, () => console.log('server started'));
+app.listen(PORT, () => console.log('server started'));
 
